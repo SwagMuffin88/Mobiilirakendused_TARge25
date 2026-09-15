c7d42feb253301ca2e34e861e493e6fc0fdaf065
Katri Lotamõis <katrilotamois@outlook.com>
2026-09-15 14:39:05 +0300

    export page components to xaml file

--- praktika_2/TreePage.xaml.cs
+++ praktika_2/TreePage.xaml.cs
@@ -3,28 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls.Shapes;
+using Microsoft.Maui.Layouts;
 
 namespace praktika_2;
 
 public partial class TreePage : ContentPage
 {
-    private BoxView _trunk;
-    private Frame _canopy;
-    private AbsoluteLayout _absoluteLayout;
-    
     public TreePage()
     {
-        _trunk = new BoxView
-        {
-            Color = Color.FromRgb(117, 89 ,62),
-            WidthRequest = 70,
-            HeightRequest = 150,
-            HorizontalOptions = LayoutOptions.Center,
-            VerticalOptions = LayoutOptions.Center
-        };
+        InitializeComponent();
+    }
 
-        _absoluteLayout = new AbsoluteLayout { Children = { _trunk } };
-
-        Content = _absoluteLayout;
+    private void OnActionClicked(object sender, EventArgs e)
+    {
+        
     }
 }
