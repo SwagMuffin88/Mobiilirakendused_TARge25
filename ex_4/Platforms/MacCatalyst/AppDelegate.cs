4aaa23f70914a60a8c9f8d846590db4e0b1a352b
Katri Lotamõis <katrilotamois@outlook.com>
2026-09-21 13:59:32 +0300

    add new exercise layout to solution

---
+++ ex_4/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ex_4;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
